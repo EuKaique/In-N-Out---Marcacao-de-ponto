2eb2c00b6164e8a4d5a119fbcb5a71b974d57f77
Kaique <34290569+EuKaique@users.noreply.github.com>
2022-08-28 17:11:59 -0300

    Validações no login

--- src/config/config.php
+++ src/config/config.php
@@ -1,16 +1,25 @@
 <?php
 
+//Define o local
 date_default_timezone_set('America/Sao_Paulo');
 setlocale(LC_TIME, 'pt_BR', 'pt_BR.utf-8', 'portuguese');
+
+//Não exibe os erros
+ini_set('display_errors', 0);
+error_reporting(E_ERROR | E_WARNING | E_PARSE); 
 
 //Pastas
 define('MODEL_PATH', realpath(dirname(__FILE__) . '/../models'));
 define('VIEW_PATH', realpath(dirname(__FILE__) . '/../views'));
+define('TEMPLATE_PATH', realpath(dirname(__FILE__) . '/../views/template'));
 define('CONTROLLER_PATH', realpath(dirname(__FILE__) . '/../controllers'));
+define('EXCEPTION_PATH', realpath(dirname(__FILE__) . '/../exceptions'));
 
 //Arquivos
 require_once(realpath(dirname(__FILE__) . '/database.php'));
 require_once(realpath(dirname(__FILE__) . '/loader.php'));
 require_once(realpath(MODEL_PATH . '/Model.php'));
+require_once(realpath(EXCEPTION_PATH . '/AppException.php'));
+require_once(realpath(EXCEPTION_PATH . '/ValidationException.php'));
 
 
--- src/models/Login.php
+++ src/models/Login.php
@@ -4,17 +4,36 @@ loadModel("User");
 
 class Login extends User
 {
+    public function validate(){
+        $errors = [];
+
+        if(!$this->email){
+            $errors['email'] = 'O campo e-mail é obrigatório';
+        }
+
+        if(!$this->password){
+            $errors['password'] = 'A senha é obrigatória';
+        }
+
+        if(count($errors) > 0){
+            throw new ValidationException($errors);
+        }
+    }
+
     public function checkLogin(){
-        $user = new User::getOne(['email' => $this->email]);
+        $this->validate();
+
+        $user = User::getOne(['email' => $this->email]);
 
         if($user){
+            if($user->end_date){
+                throw new AppException('Usuário desligado da empresa.');
+            }
             if(password_verify($this->password, $user->password)){
                 return $user;
             }
-        }else{
-            throw new Exception();
         }
-
+        throw new AppException('Usuário ou senha errada.');
         
     }
 }
