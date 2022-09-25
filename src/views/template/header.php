bc23030a456f20fd4fcfd125061eec081ac5397d
Kaique <34290569+EuKaique@users.noreply.github.com>
2022-09-25 14:18:45 -0300

    Ajustes no CSS do inNout

--- src/views/template/header.php
+++ src/views/template/header.php
@@ -10,7 +10,7 @@
     <link rel="stylesheet" href="/assets/css/template.css">
     <title>In N' Out</title>
 </head>
-<body class="hide-sidebar">
+<body>
     <header class="header">
         <div class="logo">
             <i class="icofont-travelling mr-2"></i>
@@ -25,14 +25,14 @@
         <div class="spacer"></div>
         <div class="dropdown">
             <div class="dropdown-button">
-                <span>Usuário Mock</span>
-                <i class="icofont-simple-down ml-2"></i>
+                <span class="ml-2">Usuário Mock</span>
+                <i class="icofont-simple-down mx-2"></i>
             </div>
             <div class="dropdown-content">
                 <ul class="nav-list">
                     <li class="nav-item">
                         <a href="logout.php" class="nav-link">
-                            <i class="icofont-logout mr-2"></i>
+                            <i class="icofont-logout mr-2 mt-2"></i>
                             Sair
                         </a>
                     </li>
